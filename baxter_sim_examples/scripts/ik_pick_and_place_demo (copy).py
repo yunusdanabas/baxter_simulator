e1b4c5fa180effac36d3d4b4d83baa18324ab3c1
Yunus Emre Danabas <yunusemredanabas@gmail.com>
2024-07-16 17:13:02 +0200

    Initial Commit

--- baxter_sim_examples/scripts/ik_pick_and_place_demo (copy).py
+++ baxter_sim_examples/scripts/ik_pick_and_place_demo (copy).py
@@ -181,38 +181,20 @@ class PickAndPlace(object):
         # retract to clear object
         self._retract()
 
-def load_gazebo_models(table_pose=Pose(position=Point(x=1.05, y=0.0, z=0.0)),
+def load_gazebo_models(table_pose=Pose(position=Point(x=1.0, y=0.0, z=0.0)),
                        table_reference_frame="world",
-                       block_pose=Pose(position=Point(x=0.75, y=0.0265, z=0.7825)),
-                       block_reference_frame="world", table_pose1 = Pose(position=Point(x=0.24, y= 1.19, z = 0.0)), table_reference_frame1 = "world",
-                       block_pose1 = Pose(position=Point(x= 0.624727, y=1.181466, z=0.85)), block_reference_frame1="world", block_pose2 = Pose(position=Point(x= 1.063386, y=0.368854, z=0.85)), block_reference_frame2="world", block_pose3 = Pose(position=Point(x= 0.3, y=0.8, z=0.85)), block_reference_frame3="world") :
+                       block_pose=Pose(position=Point(x=0.6725, y=0.1265, z=0.7825)),
+                       block_reference_frame="world"):
     # Get Models' Path
     model_path = rospkg.RosPack().get_path('baxter_sim_examples')+"/models/"
     # Load Table SDF
     table_xml = ''
     with open (model_path + "cafe_table/model.sdf", "r") as table_file:
         table_xml=table_file.read().replace('\n', '')
-
-   
     # Load Block URDF
     block_xml = ''
     with open (model_path + "block/model.urdf", "r") as block_file:
         block_xml=block_file.read().replace('\n', '')
-
-    # Load obstacle Block URDF
-    block_xml1 = ''
-    with open (model_path + "block/model1.urdf", "r") as block_file:
-        block_xml1=block_file.read().replace('\n', '')
-
-    # Load obstacle Block 2 URDF
-    block_xml2 = ''
-    with open (model_path + "block/model2.urdf", "r") as block_file:
-        block_xml2=block_file.read().replace('\n', '')
-
-    block_xml3 = ''
-    with open (model_path + "block/model3.urdf", "r") as block_file:
-        block_xml3=block_file.read().replace('\n', '')
-
     # Spawn Table SDF
     rospy.wait_for_service('/gazebo/spawn_sdf_model')
     try:
@@ -230,43 +212,6 @@ def load_gazebo_models(table_pose=Pose(position=Point(x=1.05, y=0.0, z=0.0)),
     except rospy.ServiceException as e:
         rospy.logerr("Spawn URDF service call failed: {0}".format(e))
 
-    # Spawn side table
-    rospy.wait_for_service('/gazebo/spawn_sdf_model')
-    try:
-        spawn_sdf = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
-        resp_sdf = spawn_sdf("cafe_table1", table_xml, "/",
-                             table_pose1, table_reference_frame1)
-    except rospy.ServiceException as e:
-        rospy.logerr("Spawn SDF service call failed: {0}".format(e))
-
-    # Spawn Obstacle Block URDF
-    rospy.wait_for_service('/gazebo/spawn_urdf_model')
-    try:
-        spawn_urdf = rospy.ServiceProxy('/gazebo/spawn_urdf_model', SpawnModel)
-        resp_urdf = spawn_urdf("block1", block_xml1, "/",
-                               block_pose1, block_reference_frame1)
-    except rospy.ServiceException as e:
-        rospy.logerr("Spawn URDF service call failed: {0}".format(e))
- 
-    # Spawn Obstacle Block 2 URDF
-    rospy.wait_for_service('/gazebo/spawn_urdf_model')
-    try:
-        spawn_urdf = rospy.ServiceProxy('/gazebo/spawn_urdf_model', SpawnModel)
-        resp_urdf = spawn_urdf("block2", block_xml2, "/",
-                               block_pose2, block_reference_frame2)
-    except rospy.ServiceException as e:
-        rospy.logerr("Spawn URDF service call failed: {0}".format(e))
-
-    # Spawn Obstacle Block 3 URDF
-    rospy.wait_for_service('/gazebo/spawn_urdf_model')
-    try:
-        spawn_urdf = rospy.ServiceProxy('/gazebo/spawn_urdf_model', SpawnModel)
-        resp_urdf = spawn_urdf("block3", block_xml3, "/",
-                               block_pose3, block_reference_frame3)
-    except rospy.ServiceException as e:
-        rospy.logerr("Spawn URDF service call failed: {0}".format(e))
-
-
 def delete_gazebo_models():
     # This will be called on ROS Exit, deleting Gazebo models
     # Do not wait for the Gazebo Delete Model service, since
@@ -275,11 +220,7 @@ def delete_gazebo_models():
     try:
         delete_model = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
         resp_delete = delete_model("cafe_table")
-        resp_delete = delete_model("cafe_table1")
         resp_delete = delete_model("block")
-        resp_delete = delete_model("block1")
-        resp_delete = delete_model("block2")
-        resp_delete = delete_model("block3")
     except rospy.ServiceException as e:
         rospy.loginfo("Delete Model service call failed: {0}".format(e))
 
@@ -303,13 +244,13 @@ def main():
     # and the IK operates with respect to the /base frame
     load_gazebo_models()
     # Remove models from the scene on shutdown
-    #rospy.on_shutdown(delete_gazebo_models)
+    rospy.on_shutdown(delete_gazebo_models)
 
     # Wait for the All Clear from emulator startup
     rospy.wait_for_message("/robot/sim/started", Empty)
 
     limb = 'left'
-    hover_distance = 0.10 # meters
+    hover_distance = 0.15 # meters
     # Starting Joint angles for left arm
     starting_joint_angles = {'left_w0': 0.6699952259595108,
                              'left_w1': 1.030009435085784,
@@ -325,138 +266,28 @@ def main():
                              y=0.999649402929,
                              z=0.00737916180073,
                              w=0.00486450832011)
-    overhead_orientation1 = Quaternion(
-                             x=-0.023772,
-                             y=0.99969,
-                             z=0.00477,
-                             w=0.004472)
-    overhead_orientation2 = Quaternion(
-                             x=-0.02377,
-                             y=0.999695,
-                             z=0.00519528,
-                             w=0.0042056)
-
-    overhead_orientation3 = Quaternion(
-                             x=-0.0230506042591,
-                             y=0.999713488515,
-                             z= 0.0063339090714,
-                             w=-0.00122152347737)
-    overhead_orientation4 = Quaternion(
-                             x=-0.018402814146,
-                             y=0.999679929408,
-                             z=0.0167710659404,
-                             w=0.00448402923228)
-
-
-    overhead_orientation5 = Quaternion(
-                             x=-0.0199118419098,
-                             y=0.999657028228,
-                             z=0.0164201194655,
-                             w=0.00444118719448)
-
-    overhead_orientation6 = Quaternion(
-                             x=-0.0253695858075,
-                             y=0.999674830555,
-                             z=0.00193555834401,
-                             w=0.00169436829301)
-
-    overhead_orientation7 = Quaternion(
-                             x=0.983984913434,
-                             y=0.17807343354,
-                             z=0.00104660181512,
-                             w=-0.00790234304179)
-
-    overhead_orientation8 = Quaternion(
-                             x=0.983438480658,
-                             y= 0.181217358123,
-                             z=0.000265163459157,
-                             w=-0.00299225062497)
-
-    overhead_orientation9 = Quaternion(
-                             x=0.983438480658,
-                             y= 0.181217358123,
-                             z=0.000265163459157,
-                             w=-0.00299225062497)
-    overhead_orientation10 = Quaternion(
-                             x=-0.0192187820803,
-                             y=0.999665020185,
-                             z=0.0169175513819,
-                             w=0.00377919156498)
-
-
     block_poses = list()
-
+    # The Pose of the block in its initial location.
+    # You may wish to replace these poses with estimates
+    # from a perception node.
     block_poses.append(Pose(
-        position=Point(x=0.777, y=0.053, z=-0.155),
+        position=Point(x=0.35059, y=0.84141, z=0.27793),
         orientation=overhead_orientation))
-    #Intermediate
+    # Feel free to add additional desired poses for the object.
+    # Each additional pose will get its own pick and place.
     block_poses.append(Pose(
-        position=Point(x=0.77956, y=0.046826, z=0.156573),
-        orientation=overhead_orientation1))
-    block_poses.append(Pose(
-        position=Point(x=0.7532, y=0.5886, z=0.16643), 
-        orientation=overhead_orientation2))
-    block_poses.append(Pose(
-        position=Point(x=0.316303736025, y=0.759593502346, z=0.212910388937), 
-        orientation=overhead_orientation3))
-    block_poses.append(Pose(
-        position=Point(x=0.0735106273962, y=0.92123359355, z= 0.0991585381435), 
-        orientation=overhead_orientation4))
-    block_poses.append(Pose(
-        position=Point(x=0.0826580581291, y=0.922486499879, z= 0.0109944819671), 
-        orientation=overhead_orientation5))
-
-
-    ######White Blocks
-
-    block_poses.append(Pose(
-        position=Point(x=0.323, y=0.82, z=-0.154),
+        position=Point(x=0.3091, y=0.81698, z=-0.005884),
         orientation=overhead_orientation))
-    block_poses.append(Pose(
-        position=Point(x=0.537911065785, y=0.452601076656, z= 0.159324510973), 
-        orientation=overhead_orientation6))
-
-    block_poses.append(Pose(
-        position=Point(x=0.729553356442, y=-0.00884255860538, z= 0.0188258286711), 
-        orientation=overhead_orientation7))
-    block_poses.append(Pose(
-        position=Point(x=0.739632306008, y=-0.0695047616031, z= 0.0744240158605), 
-        orientation=overhead_orientation8))
-    block_poses.append(Pose(
-        position=Point(x=0.739637414291, y=-0.0686413827292, z= -0.0684700961321), 
-        orientation=overhead_orientation9))
-    block_poses.append(Pose(
-        position=Point(x=0.170982446189, y=0.98737859271, z= 0.0151237846716), 
-        orientation=overhead_orientation10))
-
-
-    #Intermediate
-
     # Move to the desired starting angles
     pnp.move_to_start(starting_joint_angles)
-
-    print("Red")
-    pnp.pick(block_poses[0])
-    pnp._approach(block_poses[1])
-    pnp._approach(block_poses[2])
-    pnp._approach(block_poses[4])
-    pnp.place(block_poses[5])  
-
-  
-    print("Green")
-    pnp.pick(block_poses[6])
-    pnp._approach(block_poses[7])
-    pnp._approach(block_poses[8])
-    pnp._approach(block_poses[9])
-    pnp.place(block_poses[10])
-   
-
-
+    idx = 0
+    while not rospy.is_shutdown():
+        print("\nPicking...")
+        pnp.pick(block_poses[idx])
+        print("\nPlacing...")
+        idx = (idx+1) % len(block_poses)
+        pnp.place(block_poses[idx])
     return 0
-
-
-
-
 
 if __name__ == '__main__':
     sys.exit(main())
